a7e42226e4c382972ad8361a0120a753c699becd
RavenV8 <55328476+RavenV8@users.noreply.github.com>
2023-11-23 09:08:13 +0200

    Update LM_BLOCKCHAIN.cpp
    fixed block creation, timestamps

--- LM_BLOCKCHAIN.cpp
+++ LM_BLOCKCHAIN.cpp
@@ -1,4 +1,4 @@
-#include "headers.h"  // Assuming headers.h is in the same directory as this source file
+#include "headers.h"
 #include "LM_hash.h"
 
 class Block {
@@ -10,6 +10,16 @@ public:
     int nonce;
     int difficulty_target;
     std::vector<Transaction> transactions;
+
+    friend std::ostream& operator<<(std::ostream& os, const Block& block) {
+        os << "prev_block_hash: " << block.prev_block_hash << "\n"
+           << "timestamp: " << block.timestamp << "\n"
+           << "version: " << block.version << "\n"
+           << "merkle_root_hash: " << block.merkle_root_hash << "\n"
+           << "nonce: " << block.nonce << "\n"
+           << "difficulty_target: " << block.difficulty_target << "\n";
+        return os;
+    }
 };
 
 std::vector<Transaction> selectTransactions(std::vector<Transaction>& transactionPool) {
@@ -30,8 +40,6 @@ std::vector<Transaction> selectTransactions(std::vector<Transaction>& transactio
 
     return selectedTransactions;
 }
-
-
 
 void readingTransactions(std::vector<Transaction>& transactionPool){
     double amount;
@@ -58,23 +66,23 @@ void readingTransactions(std::vector<Transaction>& transactionPool){
 std::string calculateMerkleRoot(const std::vector<Transaction>& transactions) {
     std::vector<std::string> transactionHashes;
 
-    
+    // Extract transaction hashes
     for (const Transaction& transaction : transactions) {
         transactionHashes.push_back(transaction.transaction_id);
     }
 
-    
+    // If there are no transactions, return an empty string
     if (transactionHashes.empty()) {
         return "";
     }
 
-    
+    // Continue building the Merkle Tree until only one hash (the Merkle Root) remains
     while (transactionHashes.size() > 1) {
         std::vector<std::string> newHashes;
 
-
+        // Combine pairs of hashes and hash them together
         for (size_t i = 0; i < transactionHashes.size(); i += 2) {
-
+            // If there's an odd number of hashes, duplicate the last one
             const std::string& hash1 = transactionHashes[i];
             const std::string& hash2 = (i + 1 < transactionHashes.size()) ? transactionHashes[i + 1] : transactionHashes[i];
             std::string combinedHash = Hashing(hash1 + hash2);
@@ -82,23 +90,72 @@ std::string calculateMerkleRoot(const std::vector<Transaction>& transactions) {
             newHashes.push_back(combinedHash);
         }
 
-
+        // Replace the old hashes with the new ones
         transactionHashes = std::move(newHashes);
     }
 
+    // The last remaining hash is the Merkle Root
     return transactionHashes[0];
 }
 
+std::string calculatePreviousBlockHash(std::vector<std::string>& prevHash) {
+    if (!prevHash.empty()) {
+        return prevHash.back();
+    } else {
+        return "0000000000000000000000000000000000000000000000000000000000000000";
+    }
+}
+// Function to create a new block with the selected transactions
+Block createBlock(std::vector<std::string>& prevHash, std::vector<Transaction>& selectedTransactions) {
+    Block block;
+    
+    // Set block attributes
+    block.prev_block_hash = calculatePreviousBlockHash(prevHash);
+
+
+
+    auto currentTime = std::chrono::system_clock::now();
+    auto timestamp = std::chrono::duration_cast<std::chrono::seconds>(currentTime.time_since_epoch()).count();
+    std::time_t timestampAsTimeT = static_cast<std::time_t>(timestamp);
+    std::tm* timeinfo = std::localtime(&timestampAsTimeT);
+    char buffer[80];
+    std::strftime(buffer, sizeof(buffer), "%d %b %Y, %H:%M:%S", timeinfo);
+    block.timestamp = buffer;
+
+
+
+    block.version = 1;  // Set the version number as needed
+    block.merkle_root_hash = calculateMerkleRoot(selectedTransactions);
+
+    block.nonce = 0;  // Initial nonces
+    block.difficulty_target = 5;  // Set the difficulty target as needed
+
+    // Set block transactions
+    block.transactions = selectedTransactions;
+
+    prevHash.push_back(block.merkle_root_hash);
+    return block;
+}
+
 int main() {
-    // Perskaitomos transakcijos ir isrenkama 100 atsitiktinai
+    std::vector<std::string> prevHash;
+    
     std::vector<Transaction> transactionPool;
     readingTransactions(transactionPool);
     std::vector<Transaction> selectedTransactions = selectTransactions(transactionPool);
 
-    std::vector<Transaction> transactions;
-    std::string merkleRoot = calculateMerkleRoot(transactionPool);
-    std::cout << "Merkle Root: " << merkleRoot << std::endl;
+    std::vector<Transaction> test = selectedTransactions;
+    std::string merkleRoot = calculateMerkleRoot(test);
+    std::cout << "Merkle Root: " << merkleRoot << std::endl << std::endl;
 
+    // Create a new block with the selected transactions
+    Block newBlock = createBlock(prevHash, selectedTransactions);
+    std::cout << newBlock << std::endl;
+
+    newBlock = createBlock(prevHash, selectedTransactions);
+    std::cout << newBlock << std::endl;
+
+    // Print or handle the new block as needed
 
     return 0;
 }
